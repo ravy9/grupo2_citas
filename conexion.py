126a319401674ea5d7e1c527d6272dcdcb41b482
ravy9 <rafael.vy17@hotmail.com>
2025-06-12 08:58:47 -0500

    Version-BETA

--- conexion.py
+++ conexion.py
@@ -4,6 +4,6 @@ def get_connection():
     return psycopg2.connect(
         host="localhost",
         database="gestion_citas_db",
-        user="postgres",  # tu usuario real
-        password="rafael1517"  # tu contraseña real
+        user="postgres",
+        password="rafael1517"
     )
