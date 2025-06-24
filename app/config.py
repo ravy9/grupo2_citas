19886119998256fe61d4e40b25ac8925f90b8559
ravy9 <rafael.vy17@hotmail.com>
2025-06-24 00:08:41 -0500

    ver-alfa

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# app/config.py
+
+db_config = {
+    'host': 'localhost',
+    'user': 'postgres',
+    'password': 'rafael1517',
+    'dbname': 'gestion_citas_db',
+    'port': 5432  # por defecto en PostgreSQL
+}
--- modelo/conexion.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="gestion_citas_db",
-        user="postgres",
-        password="rafael1517"
-    )
